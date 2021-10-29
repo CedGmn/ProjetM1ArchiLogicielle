e1af0bf71acdf83625ed741560202bc0a30ec9a5
Mattéo LOPES <matteo.lopes@ynov.com>
2021-10-29 12:20:12 +0200

    Ajoutez des fichiers projet.

---
+++ Server.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Stripe;
+
+namespace StripeExample
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      WebHost.CreateDefaultBuilder(args)
+        .UseUrls("http://0.0.0.0:4242")
+        .UseWebRoot("public")
+        .UseStartup<Startup>()
+        .Build()
+        .Run();
+    }
+  }
+
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().AddNewtonsoftJson();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      // This is a sample test API key.
+      StripeConfiguration.ApiKey = "sk_test_51JppIRGEmd6jUwNMv6Ocz4GSE89T39R9R115S62GHYN9NDJlTdFFLyuN7wKIufe0mb5KbMFf3NjfwKQJiybAlDEM00TWCboaZn";
+
+      if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+      app.UseRouting();
+      app.UseStaticFiles();
+      app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+  }
+
+  [Route("create-payment-intent")]
+  [ApiController]
+  public class PaymentIntentApiController : Controller
+  {
+    [HttpPost]
+    public ActionResult Create(PaymentIntentCreateRequest request)
+    {
+      var paymentIntentService = new PaymentIntentService();
+      var paymentIntent = paymentIntentService.Create(new PaymentIntentCreateOptions
+      {
+        Amount = CalculateOrderAmount(request.Items),
+        Currency = "eur",
+        PaymentMethodTypes = new List<string>
+        {
+          "giropay",
+          "eps",
+          "p24",
+          "sofort",
+          "sepa_debit",
+          "card",
+          "bancontact",
+          "ideal",
+        },
+      });
+
+      return Json(new { clientSecret = paymentIntent.ClientSecret });
+    }
+
+    private int CalculateOrderAmount(Item[] items)
+    {
+      // Replace this constant with a calculation of the order's amount
+      // Calculate the order total on the server to prevent
+      // people from directly manipulating the amount on the client
+      return 1400;
+    }
+
+    public class Item
+    {
+      [JsonProperty("id")]
+      public string Id { get; set; }
+    }
+
+    public class PaymentIntentCreateRequest
+    {
+      [JsonProperty("items")]
+      public Item[] Items { get; set; }
+    }
+  }
+}
